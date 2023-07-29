83853621e2328b700fbeaec8b25c1d26a7473a93
janasokolovaa <123120704+janasokolovaa@users.noreply.github.com>
2023-07-29 15:22:06 +0200

    feat: add github link

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         <hr className="hr" /> */}
         <Contact />
         <hr className="hr" />
-        <p>© 2023 Alexander Stiles</p>
+        <p>© {new Date().getFullYear()} Alexander Stiles</p>
       </div>
     </div>
   );
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,17 +1,21 @@
 import React from "react";
 import linkedlogo from "../Assets/linkedin-logo.png";
 import email from "../Assets/email.png";
+import github from "../Assets/github.svg";
 
 export default function Contact() {
   return (
     <div id="contact" className="section-padding">
       <h1>CONTACT</h1>
       <div>
-        <a style={{marginRight: 10}} href="https://www.linkedin.com/in/alexander-js/">
+        <a style={{marginRight: 10}} href="https://www.linkedin.com/in/alexander-js/" target="_blank" rel="noopener noreferrer">
           <img height="40" width="40" src={linkedlogo} alt="linkedin" />
         </a>
-        <a style={{marginLeft: 10}} href="mailto: ajs5pc@virginia.edu">
+        <a style={{marginLeft: 10}} href="mailto: alexanstiles@gmail.com">
           <img height="40" width="40" src={email} alt="email" />
+        </a>
+        <a style={{marginLeft: 20}} href="https://github.com/alexanderstiles" target="_blank" rel="noopener noreferrer">
+          <img height="40" width="40" src={github} alt="github" />
         </a>
       </div>
     </div>
